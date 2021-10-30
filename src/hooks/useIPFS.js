202a7463a41f5af80c35faa36b8c3493ae3c659c
Y0moo <its.dmitrii.kpl@gmail.com>
2021-10-30 20:47:34 +0200

    edit: ipfs gateway link edited

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -1,7 +1,7 @@
 export const useIPFS = () => {
   const resolveLink = (url) => {
     if (!url.includes("ipfs://")) return url;
-    return url.replace("ipfs://", "https://ipfs.io/ipfs/");
+    return url.replace("ipfs://", "https://gateway.ipfs.io/ipfs/");
   };
 
   return { resolveLink };
