844b16fb242906107768bb25e91887e009d4ac63
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2021-10-30 21:57:34 +0200

    Added styling
    Added styling to erc20 balance component

--- src/components/ERC20Balance.jsx
+++ src/components/ERC20Balance.jsx
@@ -1,88 +1,70 @@
 import React from "react";
 import { useMoralis } from "react-moralis";
 import { useERC20Balance } from "../hooks/useERC20Balance";
-import { Flex } from "../uikit/Flex/Flex";
+import { Skeleton, Table, Image } from "antd";
 import { getEllipsisTxt } from "../utils/formatters";
 
 const styles = {
   title: {
     fontSize: "30px",
     fontWeight: "700",
-  },
-  card: {
-    padding: "20px",
-    display: "flex",
-    background: "#FFFFFF",
-    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-    border: "2px solid #e7eaf3",
-    borderRadius: "15px",
-    marginBottom: "20px",
-  },
-  table: {
-    tableLayout: "fixed",
-    width: "100%",
-    textAlign: "left",
-    borderCollapse: "collapse",
-    maxWidth: "100%",
-  },
+  }
 };
 
 function ERC20Balance(props) {
   const { assets } = useERC20Balance(props);
   const { Moralis } = useMoralis();
+  
+  const columns = [
+    {
+      title: "",
+      dataIndex: "logo",
+      key: "logo",
+      render: (logo) => <Image src={logo} alt="https://etherscan.io/images/main/empty-token.png" width="30px" height="30px"/>,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => name,
+    },
+    {
+      title: "Symbol",
+      dataIndex: "symbol",
+      key: "symbol",
+      render: (symbol) => symbol,
+    },
+    {
+      title: "Balance",
+      dataIndex: "balance",
+      key: "balance",
+      render: (value, item) => parseFloat(Moralis.Units.FromWei(value, item.decimals).toFixed(6)),
+    },
+    {
+      title: "Address",
+      dataIndex: "token_address",
+      key: "token_address",
+      render: (address) => (
+          getEllipsisTxt(address, 5)
+        ),
+    },
+  ];
 
+  let key = 0;
   return (
-    <Flex maxWidth="1200px" margin="0 15px">
+    <div>
       <h1 style={styles.title}>💰Token Balances</h1>
-      <div style={styles.card}>
-        <table style={styles.table}>
-          <thead>
-            <tr>
-              <th style={{ width: "45px" }} />
-              <th>Name</th>
-              <th>Symbol</th>
-              <th>Balance</th>
-              <th>Token Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!assets
-              ? null
-              : assets.map((item, key) => (
-                  <tr key={key}>
-                    <td>
-                      {item.logo ? (
-                        <img
-                          src={item.logo}
-                          alt={item.symbol}
-                          style={{
-                            maxWidth: "30px",
-                            maxHeight: "30px",
-                            borderRadius: "15px",
-                          }}
-                        />
-                      ) : (
-                        <img
-                          src="https://etherscan.io/images/main/empty-token.png"
-                          alt=""
-                          style={{
-                            maxWidth: "30px",
-                            maxHeight: "30px",
-                            borderRadius: "15px",
-                          }}
-                        />
-                      )}
-                    </td>
-                    <td>{item.name}</td>
-                    <td>{item.symbol}</td>
-                    <td>{Moralis.Units.FromWei(item.balance, item.decimals)}</td>
-                    <td>{getEllipsisTxt(item.token_address, 7)}</td>
-                  </tr>
-                ))}
-          </tbody>
-        </table>
-      </div>
-    </Flex>
+      <Skeleton loading={!assets}>
+        <Table
+          dataSource={assets}
+          columns={columns}
+          rowKey={(record) => {
+            key++;
+            return `${record.transaction_hash}-${key}`;
+          }}
+        />
+      </Skeleton>
+    </div>
   );
 }
 
