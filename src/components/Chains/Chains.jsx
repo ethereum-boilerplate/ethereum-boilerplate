d5d57b25f9c27c57e1abd3a880adf1a20224f2ae
Y0moo <its.dmitrii.kpl@gmail.com>
2021-10-31 14:48:40 +0200

    feat: testnets added to selectors

--- src/components/Chains/Chains.jsx
+++ src/components/Chains/Chains.jsx
@@ -23,9 +23,29 @@ const styles = {
 
 const menuItems = [
   {
-    key: "0xa86a",
-    value: "Avalanche",
-    icon: <AvaxLogo />,
+    key: "0x1",
+    value: "Ethereum",
+    icon: <ETHLogo />,
+  },
+  {
+    key: "0x3",
+    value: "Ropsten Testnet",
+    icon: <ETHLogo />,
+  },
+  {
+    key: "0x4",
+    value: "Rinkeby Testnet",
+    icon: <ETHLogo />,
+  },
+  {
+    key: "0x2a",
+    value: "Kovan Testnet",
+    icon: <ETHLogo />,
+  },
+  {
+    key: "0x5",
+    value: "Goerli Testnet",
+    icon: <ETHLogo />,
   },
   {
     key: "0x38",
@@ -33,14 +53,24 @@ const menuItems = [
     icon: <BSCLogo />,
   },
   {
-    key: "0x1",
-    value: "Ethereum",
-    icon: <ETHLogo />,
+    key: "0x61",
+    value: "Smart Chain Testnet",
+    icon: <BSCLogo />,
   },
   {
     key: "0x89",
     value: "Polygon",
     icon: <PolygonLogo />,
+  },
+  {
+    key: "0x13881",
+    value: "Mumbai",
+    icon: <PolygonLogo />,
+  },
+  {
+    key: "0xa86a",
+    value: "Avalanche",
+    icon: <AvaxLogo />,
   },
 ];
 
