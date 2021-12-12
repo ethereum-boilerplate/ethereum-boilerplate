62d1bcaf37396d626f6ac97dbffabcc791b82f12
Roy <toledoroy@gmail.com>
2021-12-12 10:54:36 -0800

    Validate .env file + detaild error message

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const Application = () => {
   const isServerInfo = APP_ID && SERVER_URL ? true : false;
+  //Validate
+  if(!APP_ID || !SERVER_URL) throw new Error("Missing Moralis Application ID or Server URL. Make sure to set your .env file.");
   if (isServerInfo)
     return (
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
