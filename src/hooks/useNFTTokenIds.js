dddb875498f3ab3b6b8b39afc4665466ac1e0b96
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-26 01:18:03 +0000

    simplifying code

--- src/hooks/useNFTTokenIds.js
+++ src/hooks/useNFTTokenIds.js
@@ -39,37 +39,10 @@ export const useNFTTokenIds = (addr) => {
                 const NFTs = data.result;
                 setTotalNFTs(data.total);
                 setFetchSuccess(true);
-                for (let NFT of NFTs) {
+                for (const NFT of NFTs) {
                     if (NFT?.metadata) {
                         NFT.metadata = JSON.parse(NFT.metadata);
                         NFT.image = resolveLink(NFT.metadata?.image);
-                    } else if (NFT?.token_uri) {
-                        try {
-                            await fetch(NFT.token_uri)
-                                .then((response) => response.json())
-                                .then((data) => {
-                                    NFT.image = resolveLink(data.image);
-                                });
-                        } catch (error) {
-                            setFetchSuccess(false);
-
-                            /*          !!Temporary work around to avoid CORS issues when retrieving NFT images!!
-                                        Create a proxy server as per https://dev.to/terieyenike/how-to-create-a-proxy-server-on-heroku-5b5c
-                                        Replace <your url here> with your proxy server_url below
-                                        Remove comments :)
-                            
-                                          try {
-                                            await fetch(`<your url here>/${NFT.token_uri}`)
-                                            .then(response => response.json())
-                                            .then(data => {
-                                              NFT.image = resolveLink(data.image);
-                                            });
-                                          } catch (error) {
-                                            setFetchSuccess(false);
-                                          }
-                            
-                             */
-                        }
                     }
                 }
                 setNFTTokenIds(NFTs);
