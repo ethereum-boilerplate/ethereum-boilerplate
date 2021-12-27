60893a18bb7b55971f2abdd3a26057a10b78d22b
Martin Kuzdowicz (CoderDidit) <86926500+coderdidit@users.noreply.github.com>
2021-12-27 15:46:15 +0000

    styling (#22)
    Co-authored-by: mkuzdowicz <martin.kuzdowicz@gmail.com>

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 function Transactions() {
     return (
-        <>
+        <div style={{
+            fontFamily: "Source Serif Pro, sans-serif",
+        }}>
             <Text strong>
                 <h1>Your Transactions Comming soon! 🚀</h1>
             </Text>
-        </>
+        </div>
     );
 };
 export default Transactions;
