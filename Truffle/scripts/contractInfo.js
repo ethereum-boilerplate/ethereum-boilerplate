18a63cbf3670562d971ced7b30117bf1d507cb3a
dguard <av.serditov@gmail.com>
2021-12-29 17:14:45 +0500

    fix name of file MetaCoin.json

--- Truffle/scripts/contractInfo.js
+++ Truffle/scripts/contractInfo.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-fs.copyFile("build/contracts/Metacoin.json", "../src/contracts/contractInfo.json", (err) => {
+fs.copyFile("build/contracts/MetaCoin.json", "../src/contracts/contractInfo.json", (err) => {
   if (err) throw err;
   console.log("✅ Your contract's ABI was copied to the frontend");
 });
