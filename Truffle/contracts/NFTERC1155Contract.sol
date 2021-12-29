b1aadb364abb38569be4c049a3df2a63c1e21a21
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-29 15:05:12 +0000

    nft contract deployment

--- Truffle/contracts/NFTERC1155Contract.sol
+++ Truffle/contracts/NFTERC1155Contract.sol
@@ -5,17 +5,19 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract NFTContract is ERC1155, Ownable {
-    uint256 public constant ARTWORK = 0;
-    uint256 public constant PHOTO = 1;
+    uint256 public constant ERACH = 0;
+    uint256 public constant ORLAI = 1;
+    uint256 public constant ODIALT = 2;
     address marketplaceAddress;
 
     constructor(address _marketplaceAddress)
-        ERC1155("https://inzloc1b6zrv.usemoralis.com/{id}.json")
+        ERC1155("https://ipfs.moralis.io:2053/ipfs/QmYUsDTWGCZzfJRosYgLcA5DdiiJpnu4WquWZVZNki25PZ/metadata/{id}.json")
     {
         marketplaceAddress = _marketplaceAddress;
         // autmoatically mint tokens to the sender
-        _mint(msg.sender, ARTWORK, 5, "");
-        _mint(msg.sender, PHOTO, 5, "");
+        _mint(msg.sender, ERACH, 10, "");
+        _mint(msg.sender, ORLAI, 10, "");
+        _mint(msg.sender, ODIALT, 10, "");
     }
 
     function mint(
@@ -48,3 +50,6 @@ contract NFTContract is ERC1155, Ownable {
 // last deployment on rinkeby testnet: 0x6d996636c3917d8D0B99Dc1D877Fbf7fC189c8f1
 
 // last deployment on rinkeby testnet: 0x7650D3448F8044d8732528148c2A2d6B1D17BA88
+
+// latest contract deployment on rinkeby 0x7ae9f5b997dacd922e2da3fd79207d01ee6f5300
+
