c13702227435183fca9abf24278c11c2caec4580
vincentblockchainnft <vcj.kleiman@gmail.com>
2021-12-30 16:20:03 +0100

    Create .env.ex

---
+++ .env.ex
@@ -0,0 +1,11 @@
+# Mandatory info for starting the app
+REACT_APP_MORALIS_APPLICATION_ID = 2cmRPq6EUqZG41tM0Ou61L8HUTp0Roz6AFcLvhqC
+REACT_APP_MORALIS_SERVER_URL = https://advogplqnmfz.usemoralis.com:2053/server
+
+# Optional info for connecting your localChain and Moralis Database
+moralisApiKey = xxxxx
+moralisApiSecret = xxxxxxx
+frpcPath = F:\frpc\frpc.exe
+chain = ganache
+moralisSubdomain = xxxxxxx.usemoralis.com
+abiPath = "F:\ethereum-boilerplate\Truffle\build\contracts\Contract.json"
