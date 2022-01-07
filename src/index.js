c0e79c9de1124d28fe65d695bf76ee4c45bc5c69
Rob R <hi.buyim@gmail.com>
2022-01-07 18:33:10 +0200

    updated to latest react-router-dom

--- src/index.js
+++ src/index.js
@@ -12,32 +12,32 @@ const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const Application = () => {
-  const isServerInfo = APP_ID && SERVER_URL ? true : false;
-  //Validate
-  if (!APP_ID || !SERVER_URL)
-    throw new Error(
-      "Missing Moralis Application ID or Server URL. Make sure to set your .env file."
-    );
-  if (isServerInfo)
-    return (
-      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <App isServerInfo />
-      </MoralisProvider>
-    );
-  else {
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <QuickStart />
-      </div>
-    );
-  }
+	const isServerInfo = APP_ID && SERVER_URL ? true : false;
+	//Validate
+	if (!APP_ID || !SERVER_URL)
+		throw new Error(
+			"Missing Moralis Application ID or Server URL. Make sure to set your .env file."
+		);
+	if (isServerInfo)
+		return (
+			<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+				<App isServerInfo />
+			</MoralisProvider>
+		);
+	else {
+		return (
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<QuickStart />
+			</div>
+		);
+	}
 };
 
 ReactDOM.render(
-  <StrictMode>
-    <Application />,
-  </StrictMode>,
-  document.getElementById("root")
+	<StrictMode>
+		<Application />,
+	</StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
