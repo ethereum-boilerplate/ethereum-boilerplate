052f52fe09d4f8e3612877a0e09fa56d2da6f677
Martin Kuzdowicz (CoderDidit) <86926500+coderdidit@users.noreply.github.com>
2022-01-10 01:38:02 +0000

    Add Home page content + favicon update (#50)
    * home page content update
    
    * updated favicon
    
    Co-authored-by: mkuzdowicz <martin.kuzdowicz@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
-import { Card, Typography } from "antd";
+import { Card, Typography, Divider } from "antd";
 import React from "react";
 import { Button } from 'antd';
 import { Row, Col } from 'antd';
 import { brightFontCol } from "GlobalStyles";
 import { Link } from "react-router-dom";
 import { BtnPrimary } from "../GlobalStyles";
+import {
+  SmileFilled, VideoCameraFilled,
+  WalletFilled, SkinFilled
+} from "@ant-design/icons";
 
 const { Text } = Typography;
 
@@ -117,30 +121,61 @@ export default function Home() {
         </Col>
       </Row>
       <Row style={{
-        fontFamily: "Source Serif Pro",
         flexBasis: "100%",
-        justifyContent: "center",
-        marginTop: "6rem",
+        marginTop: "4rem",
+        marginBottom: "2rem"
       }}>
-        <Col span={100} style={{
-          textAlign: "center",
-        }}>
-          <h1>Roadmap</h1>
-          ...
+        <Col span={100} >
+          <h1 style={{
+            fontFamily: "Source Serif Pro",
+            fontSize: "25px",
+            padding: "1rem",
+          }}>How it works?</h1>
+          <div style={{
+            fontSize: "18px",
+          }}>
+            <ol style={{
+              listStyle: "none",
+              textAlign: "left",
+              padding: "1rem",
+              margin: 0,
+            }}>
+              <li>1. Connect your wallet&nbsp;&nbsp;
+                <WalletFilled style={{ color: "#CE6527" }} /></li>
+              <li>2. Select or buy your NFT avatar&nbsp;&nbsp;
+                <SkinFilled style={{ color: "#64B1FF" }} /></li>
+              <li>3. Enable your Webcam&nbsp;&nbsp;
+                <VideoCameraFilled style={{ color: "#4957CB" }} /></li>
+              <li>4. Join MetaGymLand&nbsp;&nbsp;🎉</li>
+            </ol>
+            <div style={{
+              padding: "1rem",
+            }}>Or you can skip steps 1 and 2 for now&nbsp;&nbsp;
+              <SmileFilled style={{ color: "#FFBE59" }} />&nbsp;&nbsp;
+              By clicking&nbsp;&nbsp;&nbsp;
+              <Button style={BtnPrimary}>Try with Demo Avatar</Button>
+            </div>
+          </div>
         </Col>
       </Row>
+      <Divider style={{
+        backgroundColor: "#032139",
+      }}></Divider>
       <Row style={{
-        fontFamily: "Source Serif Pro",
         flexBasis: "100%",
-        justifyContent: "center",
-        marginTop: "12rem",
-        marginBottom: "10rem"
+        marginTop: "3rem",
+        marginBottom: "3rem"
       }}>
+
         <Col span={100} style={{
           textAlign: "center"
         }}>
-          <h1>Team</h1>
-          ...
+          <div style={{
+            fontSize: "18px",
+          }}>
+            Want to be notified about our progress and future plans?&nbsp;&nbsp;&nbsp;
+            <Button type="primary" style={BtnPrimary}>Sing Up to MetaGymLand Newsletter</Button>
+          </div>
         </Col>
       </Row>
     </div>
