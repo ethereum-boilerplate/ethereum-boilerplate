f8a1a93d70dd69bcaea2c2abdeff391d0705090d
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-12 00:58:00 +0000

    webcam drawing improvements

--- src/components/Webcam/pose-drawing.js
+++ src/components/Webcam/pose-drawing.js
@@ -9,8 +9,20 @@ const IDLE_POSE_LANDMARKS_COLOR = "#FF0000";
 const IDLE_POSE_LINES_COLOR = "#00FF00";
 const VisibilityMin = ConfidenceScore;
 
+const drawLine = (p1, p2, color, ctx, width, height) => {
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(p1.x * width, p1.y * height)
+    ctx.lineTo(p2.x, p2.y * height);
+    ctx.stroke();
+};
+
 export const drawPose = (canvasRef, results) => {
     // Get Canvas
+    const width = canvasRef.current.width;
+    const height = canvasRef.current.height;
     const canvasCtx = canvasRef.current.getContext("2d");
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
@@ -33,6 +45,13 @@ export const drawPose = (canvasRef, results) => {
     // Draw Pose mesh
     canvasCtx.globalCompositeOperation = 'source-over';
     if (results.poseLandmarks) {
+        // console.log('results', results);
+        const nose = results.poseLandmarks[0];
+
+        drawLine(nose, { x: 0, y: nose.y }, "#1990FF", canvasCtx, width, height);
+        // path from nose to left end   
+        drawLine(nose, { x: canvasRef?.current.width, y: nose.y }, "#20BF96", canvasCtx, width, height);
+
         drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
             {
                 color: IDLE_POSE_LINES_COLOR,
@@ -44,51 +63,52 @@ export const drawPose = (canvasRef, results) => {
         drawLandmarks(canvasCtx, Object.values(POSE_LANDMARKS_LEFT)
             .map(index => results.poseLandmarks[index]),
             {
-                color: IDLE_POSE_LANDMARKS_COLOR,
-                lineWidth: 4,
+                color: 'black', fillColor: 'white',
+                lineWidth: 1,
                 visibilityMin: VisibilityMin,
             });
         // right
         drawLandmarks(canvasCtx, Object.values(POSE_LANDMARKS_RIGHT)
             .map(index => results.poseLandmarks[index]),
             {
-                color: 'rgb(0,217,231)',
-                lineWidth: 4,
+                color: 'black', fillColor: 'white',
+                lineWidth: 1,
                 visibilityMin: VisibilityMin,
             });
         // neutral
         drawLandmarks(canvasCtx, Object.values(POSE_LANDMARKS_NEUTRAL)
             .map(index => results.poseLandmarks[index]),
             {
-                color: 'white', fillColor: 'rgb(0,217,231)',
-                lineWidth: 4,
+                color: 'black', fillColor: 'white',
+                lineWidth: 1,
                 visibilityMin: VisibilityMin,
             });
 
         const le = {
             // LEFT_EYE_INNER: 1, 
             LEFT_EYE: 2,
-            LEFT_EYE_OUTER: 3
+            // LEFT_EYE_OUTER: 3
         }
 
         drawLandmarks(canvasCtx, Object.values(le)
             .map(index => results.poseLandmarks[index]),
             {
-                color: 'white', fillColor: 'black',
-                lineWidth: 4,
+                color: '#2450F7', fillColor: '#2450F7',
+                lineWidth: 20,
                 visibilityMin: VisibilityMin,
             });
 
         const re = {
             // RIGHT_EYE_INNER: 4, 
-            RIGHT_EYE: 5, RIGHT_EYE_OUTER: 6
+            RIGHT_EYE: 5,
+            // RIGHT_EYE_OUTER: 6
         }
 
         drawLandmarks(canvasCtx, Object.values(re)
             .map(index => results.poseLandmarks[index]),
             {
-                color: 'white', fillColor: 'black',
-                lineWidth: 4,
+                color: '#2450F7', fillColor: '#2450F7',
+                lineWidth: 20,
                 visibilityMin: VisibilityMin,
             });
     }
