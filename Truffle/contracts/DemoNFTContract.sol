1f15c856e4ad244a3df64eea628f93c25bf39bab
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-13 23:48:36 +0000

    deploying demo nft

--- Truffle/contracts/DemoNFTContract.sol
+++ Truffle/contracts/DemoNFTContract.sol
@@ -15,7 +15,7 @@ contract DemoNFTContract is ERC721URIStorage, Ownable {
         marketplaceAddress = _marketplaceAddress;
         // automatically mint tokens to the contract deployer
         createToken(
-            "https://ipfs.moralis.io:2053/ipfs/QmP2ZgdESy2Ji3G5qD8s1AZNgTDPtunJXASAbWNjk8vidk/metadata/0000000000000000000000000000000000000000000000000000000000000003.json"
+            "https://ipfs.moralis.io:2053/ipfs/QmeBjJN594ZqAnTcR6oaQe7GP2svbzz3ytiMnyJgxCH8bR/metadata/0000000000000000000000000000000000000000000000000000000000000003.json"
         );
     }
 
@@ -28,3 +28,5 @@ contract DemoNFTContract is ERC721URIStorage, Ownable {
 }
 
 // latest contract deployment on rinkeby: 0x4bD789C6D44F2e2f9963a5eb26D3b34D2eE42307
+// latest contract deployment on rinkeby: 0xa7f993f90ffaaa400c6360ef1e2017113c5ced91
+// latest contract deployment on rinkeby: 0xCB14609cCcE601fC24A7b39F39E0609b0B62acE4
