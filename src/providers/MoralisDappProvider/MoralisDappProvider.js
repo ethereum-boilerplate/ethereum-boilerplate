5f39558bb0f59c655c3a69ec205663e7a49c9151
Seatea01 <seateannu@gmail.com>
2022-01-22 01:40:10 +0000

    ok

--- src/providers/MoralisDappProvider/MoralisDappProvider.js
+++ src/providers/MoralisDappProvider/MoralisDappProvider.js
@@ -6,6 +6,10 @@ function MoralisDappProvider({ children }) {
   const { web3, Moralis, user } = useMoralis();
   const [walletAddress, setWalletAddress] = useState();
   const [chainId, setChainId] = useState();
+  const [contractABI, setContractABI] = useState('[ { "inputs": [ { "internalType": "address", "name": "nftContract", "type": "address" },{ "internalType": "uint256", "name": "tokenId", "type": "uint256" },{"internalType": "uint256","name": "price", "type": "uint256" }],"name": "createMarketItem", "outputs": [],"stateMutability": "payable","type": "function"}, { "inputs": [  {"internalType": "address", "name": "nftContract","type": "address"}, { "internalType": "uint256","name": "itemId","type": "uint256"}],"name": "createMarketSale","outputs": [],"stateMutability": "payable", "type": "function"},{ "inputs": [], "stateMutability": "nonpayable","type": "constructor"},{ "anonymous": false,"inputs": [{ "indexed": true,"internalType": "uint256","name": "itemId","type": "uint256"},{ "indexed": true,"internalType": "address", "name": "nftContract","type": "address"},{"indexed": true,"internalType": "uint256", "name": "tokenId","type": "uint256"}, { "indexed": false, "internalType": "address", "name": "seller","type": "address" }, {"indexed": false, "internalType": "address", "name": "owner", "type": "address" },{"indexed": false,"internalType": "uint256","name": "price","type": "uint256" }, {"indexed": false, "internalType": "bool","name": "sold","type": "bool" } ], "name": "MarketItemCreated", "type": "event"}, {"anonymous": false, "inputs": [{ "indexed": true,"internalType": "uint256", "name": "itemId", "type": "uint256" }, { "indexed": false, "internalType": "address","name": "owner", "type": "address"} ],"name": "MarketItemSold","type": "event" }, {"inputs": [], "name": "fetchMarketItems","outputs": [ { "components": [{"internalType": "uint256","name": "itemId","type": "uint256"}, {"internalType": "address","name": "nftContract","type": "address" }, {"internalType": "uint256","name": "tokenId", "type": "uint256"}, {"internalType": "address payable","name": "seller", "type": "address" }, {"internalType": "address payable","name": "owner","type": "address"},{ "internalType": "uint256","name": "price","type": "uint256"}, {"internalType": "bool","name": "sold", "type": "bool" } ], "internalType": "struct marketPlaceBoilerPlate.MarketItem[]","name": "", "type": "tuple[]"}],"stateMutability": "view","type": "function"},{ "inputs": [], "name": "owner","outputs": [ { "internalType": "address","name": "", "type": "address"}],"stateMutability": "view", "type": "function" }]' );
+
+  const [marketAddress, setMarketAddress] = useState("0x15B7a6A97bc769D6F43d8e7E36C0B9277755C189");
+  
   useEffect(() => {
     Moralis.onChainChanged(function (chain) {
       setChainId(chain);
@@ -25,7 +29,7 @@ function MoralisDappProvider({ children }) {
   );
 
   return (
-    <MoralisDappContext.Provider value={{ walletAddress, chainId }}>
+    <MoralisDappContext.Provider value={{ walletAddress, chainId, contractABI, setContractABI, marketAddress, setMarketAddress }}>
       {children}
     </MoralisDappContext.Provider>
   );
