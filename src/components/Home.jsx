1e8aba972548310a24da1ee9499abd691ea07c64
Martin Kuzdowicz (CoderDidit) <86926500+coderdidit@users.noreply.github.com>
2022-01-26 19:20:34 +0000

    using more css grid in home page (#85)
    * using more css grid in home page
    
    * better html semantics
    
    * improvements
    
    * version top-up
    
    Co-authored-by: mkuzdowicz <martin.kuzdowicz@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Card, Typography, Divider } from "antd";
+import { Typography, Divider } from "antd";
 import React from "react";
 import { Button, Image } from 'antd';
 import {
@@ -34,7 +34,7 @@ const styles = {
   }
 };
 
-const LRPadding = "5rem";
+const LRPadding = "6rem";
 
 export default function Home() {
   return (
@@ -44,147 +44,141 @@ export default function Home() {
         paddingRight: LRPadding,
       }}
     >
-      <div style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
+      <section style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
       }}>
-        <Card
-          style={styles.card}
-          title={
-            <>
-              <Text strong style={styles.titleText}>
-                Ready to get started?
-              </Text>
-            </>
-          }
-        >
-          <div style={{
+        <div style={{
+          padding: "2rem 1rem",
+        }}>
+          <div strong style={{
+            ...styles.titleText,
+            paddingTop: "1rem",
           }}>
-            <Text style={styles.text}>
-              Follow steps below, have fun and get fit!
-            </Text>
-            <div
+            Ready to get started?
+          </div>
+          <div style={{
+            ...styles.text,
+            paddingTop: "1rem",
+          }}>
+            Follow steps below, have fun and get fit!
+          </div>
+
+          <div style={{
+            paddingTop: "4.5rem",
+          }}>
+            <Button
+              type="primary"
               style={{
-                display: "flex",
-                gap: "1rem",
-                justifyContent: "left",
-                alignItems: "center",
-                marginTop: "5rem",
+                ...BtnPrimary,
+                marginRight: "1rem",
               }}
             >
-              <Button
-                type="primary"
-                style={{
-                  ...BtnPrimary,
-                }}
-              >
-                <Link to="/avatars">
-                  Play now
-                </Link>
-              </Button>
-              <Button style={{
-                ...BtnInfo,
-              }}>
-                <Link to="/demo-avatar">
-                  Try with Demo Avatar
-                </Link>
-              </Button>
-            </div>
+              <Link to="/avatars">
+                Play now
+              </Link>
+            </Button>
+            <Button style={{
+              ...BtnInfo,
+            }}>
+              <Link to="/demo-avatar">
+                Try with Demo Avatar
+              </Link>
+            </Button>
           </div>
-        </Card>
-        <Card
-          style={{
-            ...styles.card,
-          }}
-        >
-          <div>
-            <Image
-              preview={false}
-              src={mglVideoDemoImg}
-              alt=""
-              className="demo-video"
-              style={{
-                width: "720px",
-                padding: "0px",
-                margin: "0px",
-              }}
-              onClick={() =>
-                window.open(
-                  `https://www.youtube.com/watch?v=Bv7i0m6Szzc`,
-                  "_blank"
-                )
-              }
-            />
-          </div>
-        </Card>
-      </div>
+        </div>
+
+        <div>
+          <Image
+            preview={false}
+            src={mglVideoDemoImg}
+            alt=""
+            className="demo-video"
+            style={{
+              width: "100%",
+              padding: "0px",
+              margin: "0px",
+            }}
+            onClick={() =>
+              window.open(
+                `https://www.youtube.com/watch?v=Bv7i0m6Szzc`,
+                "_blank"
+              )
+            }
+          />
+        </div>
+      </section>
+
       <div style={{
         flexBasis: "100%",
       }} />
-      <div style={{
-        padding: "0.5rem 1rem 3rem 1rem",
-      }}>
-        <Text strong style={pageTitle2Style}>
-          How it works?
-        </Text>
-      </div>
-      <div>
-        <Image
-          preview={false}
-          src={howItWorksImg}
-          alt=""
-          style={{
-          }}
-        />
-      </div>
+      <section>
+        <div style={{
+          padding: "0.5rem 1rem 3rem 1rem",
+        }}>
+          <Text strong style={pageTitle2Style}>
+            How it works?
+          </Text>
+        </div>
+        <div>
+          <Image
+            preview={false}
+            src={howItWorksImg}
+            alt=""
+            style={{
+            }}
+          />
+        </div>
+      </section>
       <div style={{
         flexBasis: "100%",
       }} />
       <Divider style={{
         backgroundColor: "#032139",
       }}></Divider>
-      <div style={{
-        marginTop: "3rem",
-        marginBottom: "3rem",
-        alignItems: "center",
-        justifyContent: "center"
-      }}>
+      <section>
         <div style={{
-          textAlign: "center"
+          marginTop: "3rem",
+          marginBottom: "3rem",
+          alignItems: "center",
+          justifyContent: "center"
         }}>
           <div style={{
-            ...descriptionStyle,
+            textAlign: "center"
           }}>
-            Want to be notified about our progress and future plans?&nbsp;&nbsp;&nbsp;
-            <Button type="primary"
-              style={BtnPrimary}
-              onClick={() =>
-                window.open(
-                  `https://coderdidit.ck.page/666d446d94`,
-                  "_blank"
-                )
-              }
-            >
-              Sing up to newsletter
-            </Button>
+            <div style={{
+              ...descriptionStyle,
+            }}>
+              Want to be notified about our progress and future plans?&nbsp;&nbsp;&nbsp;
+              <Button type="primary"
+                style={BtnPrimary}
+                onClick={() =>
+                  window.open(
+                    `https://coderdidit.ck.page/666d446d94`,
+                    "_blank"
+                  )
+                }
+              >
+                Sing up to newsletter
+              </Button>
+            </div>
+          </div>
+          <div style={{
+            flexBasis: "100%",
+          }} />
+          <div style={{
+            marginTop: "2rem",
+            textAlign: "center",
+            fontSize: "18px",
+          }}>
+            <span style={{
+              opacity: 0.2,
+            }}>contact:</span>
+            &nbsp;&nbsp;
+            <span>coderdidit@gmail.com</span>
           </div>
         </div>
-        <div style={{
-          flexBasis: "100%",
-        }} />
-        <div style={{
-          marginTop: "2rem",
-          textAlign: "center",
-          fontSize: "18px",
-        }}>
-          <span style={{
-            opacity: 0.2,
-          }}>contact:</span>
-          &nbsp;&nbsp;
-          <span>coderdidit@gmail.com</span>
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
