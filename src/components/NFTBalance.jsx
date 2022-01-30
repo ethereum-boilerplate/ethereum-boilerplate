c6af7101bdcb701a9500e7f8b9075c311c81ce42
michaeljwright <mike.filmworks@gmail.com>
2022-01-30 20:09:55 +0000

    Initial test update for Unlockable content
    bit of a god class now, need to split it out into several components later

--- src/components/NFTBalance.jsx
+++ src/components/NFTBalance.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import { useMoralis, useNFTBalances } from "react-moralis";
-import { Card, Image, Tooltip, Modal, Input, Skeleton } from "antd";
+import {
+  Card,
+  Image,
+  Tooltip,
+  Modal,
+  Input,
+  Skeleton,
+  Typography,
+  Button,
+  Divider,
+  notification,
+} from "antd";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import {
   FileSearchOutlined,
-  SendOutlined,
-  ShoppingCartOutlined,
+  LockOutlined,
+  UnlockOutlined,
 } from "@ant-design/icons";
 import { getExplorer } from "helpers/networks";
-import AddressInput from "./AddressInput";
+// import AddressInput from "./AddressInput";
 import { useVerifyMetadata } from "hooks/useVerifyMetadata";
+import { useIfContractOwner } from "hooks/useIfContractOwner";
 
 const { Meta } = Card;
+
+const { Text } = Typography;
 
 const styles = {
   NFTs: {
@@ -23,20 +39,55 @@ const styles = {
     width: "100%",
     gap: "10px",
   },
+  title: {
+    fontSize: "18px",
+    fontWeight: "800",
+  },
+  subTitle: {
+    fontSize: "16px",
+    fontWeight: "700",
+  },
+  text: {
+    fontSize: "16px",
+  },
+  input: {
+    margin: "10px 0 0 0",
+  },
+  icon: {
+    margin: "0 5px 0 0",
+  },
+  card: {
+    margin: "0 10px 10px auto",
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "0.5rem",
+  },
+  timeline: {
+    marginBottom: "-45px",
+  },
+  boundary: {
+    display: "flex",
+    margin: "10px",
+    padding: "10px",
+  },
 };
 
 function NFTBalance() {
   const { data: NFTBalances } = useNFTBalances();
-  const { Moralis, chainId } = useMoralis();
-  const [visible, setVisibility] = useState(false);
-  const [receiverToSend, setReceiver] = useState(null);
-  const [amountToSend, setAmount] = useState(null);
-  const [nftToSend, setNftToSend] = useState(null);
+  const { Moralis, chainId, account } = useMoralis();
+  const [lockVisibility, setLockVisibility] = useState(false);
+  const [createContentVisibility, setCreateContentVisibility] = useState(false);
+  // const [receiverToSend, setReceiver] = useState(null);
+  // const [amountToSend, setAmount] = useState(null);
+  const [lockContent, setLockContent] = useState(null);
+  const [editorSummaryContent, setEditorSummaryContent] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { verifyMetadata } = useVerifyMetadata();
+  // const { isContractOwner } = useIfContractOwner();
+  // let contractOwner = null;
 
   async function transfer(nft, amount, receiver) {
-    console.log(nft, amount, receiver);
+    // console.log(nft, amount, receiver);
     const options = {
       type: nft?.contract_type?.toLowerCase(),
       tokenId: nft?.token_id,
@@ -60,29 +111,204 @@ function NFTBalance() {
     }
   }
 
-  const handleTransferClick = (nft) => {
-    setNftToSend(nft);
-    setVisibility(true);
+  const checkContractOwnership = async (nft) => {
+    setIsPending(true);
+    const walletAddress = nft?.owner_of;
+    const contractAddress = nft?.token_address;
+    console.log("walletAddress: " + walletAddress);
+    console.log("contractAddress: " + contractAddress);
+    console.log("chainId: " + chainId);
+
+    if (walletAddress && contractAddress && chainId) {
+      try {
+        const options = {
+          address: contractAddress,
+          chain: chainId,
+          topic0:
+            "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
+        };
+
+        try {
+          const contractOwners = await Moralis.Web3API.native.getLogsByAddress(
+            options
+          );
+
+          setIsPending(false);
+
+          const contractOwnerAddress = contractOwners?.result[0]?.topic2;
+          if (contractOwnerAddress) {
+            console.log(
+              "contractOwnerAddress: " +
+                contractOwnerAddress.replace("0x000000000000000000000000", "0x")
+            );
+          }
+
+          if (
+            contractOwnerAddress &&
+            walletAddress &&
+            contractOwnerAddress.replace("0x000000000000000000000000", "0x") ===
+              walletAddress
+          ) {
+            setLockVisibility(false);
+            setCreateContentVisibility(true);
+            console.log(`Owner of contract`);
+          } else {
+            setLockVisibility(false);
+            openNotification(
+              "error",
+              "Not owner of contract",
+              "Sorry, but you are not the owner of the contract"
+            );
+            console.log(`Not owner of contract`);
+          }
+        } catch (e) {
+          console.log(e);
+          openNotification("error", "An error occurred", e.message);
+        }
+      } catch (e) {
+        console.log(e);
+        openNotification("error", "An error occurred", e.message);
+      }
+    }
+    setIsPending(false);
   };
 
-  const handleChange = (e) => {
-    setAmount(e.target.value);
+  const createUnlockableContent = (content) => {
+    setIsPending(true);
+
+    // check if content is empty and if already exists??
+
+    try {
+      const newCollection = Moralis.Object.extend("Collections");
+      const collection = new newCollection();
+      collection.set("contractAddress", lockContent.token_address);
+      collection.set("walletAddress", lockContent.owner_of);
+      collection.set("content", content);
+      collection.save();
+
+      setIsPending(false);
+      setCreateContentVisibility(false);
+      openNotification(
+        "info",
+        "Content created",
+        "Your Unlockable content has been saved and connected to your contract"
+      );
+    } catch (e) {
+      alert(e.message);
+      setIsPending(false);
+    }
   };
 
-  console.log("NFTBalances", NFTBalances);
+  const handleLockContentClick = (nft = null) => {
+    setLockContent(nft);
+    setLockVisibility(true);
+  };
+
+  const handleLockContentChange = (e) => {
+    setLockContent({
+      owner_of: account,
+      token_address: e.target.value,
+    });
+  };
+
+  const openNotification = (type, title, message) => {
+    notification[type]({
+      message: title,
+      description: message,
+    });
+  };
+
+  const clearLocalStorage = () => {
+    window.localStorage.clear();
+  };
+
+  // console.log("NFTBalances: ", NFTBalances);
+  // console.log("account: ", account);
+
   return (
-    <div style={{ padding: "15px", maxWidth: "1030px", width: "100%" }}>
-      <h1>🖼 NFT Balances</h1>
+    <div style={styles.boundary}>
+      <Card
+        style={styles.card}
+        title={
+          <>
+            <Text style={styles.title} strong>
+              My NFTs
+            </Text>
+          </>
+        }
+      >
+        <Card
+          style={styles.card}
+          title={
+            <>
+              <Text style={styles.subTitle}>Unlock content</Text>
+            </>
+          }
+        >
+          <Text>
+            {" "}
+            <UnlockOutlined style={styles.icon} /> Click the "Unlock" icon next
+            to each of your NFTs to reveal unlockable content
+          </Text>
+        </Card>
+
+        <Card
+          style={styles.card}
+          title={
+            <>
+              <Text style={styles.subTitle}>Lock content</Text>
+            </>
+          }
+        >
+          <Text>
+            <Button
+              type="primary"
+              icon={<LockOutlined />}
+              onClick={() => handleLockContentClick()}
+            >
+              Enter Contract Address
+            </Button>
+          </Text>
+          <Divider dashed />
+          <Text>
+            {" "}
+            <LockOutlined style={styles.icon} /> Or, click the "Lock" icon next
+            to each NFT to create unlockable content (will display if you own
+            it)
+          </Text>
+        </Card>
+      </Card>
       <div style={styles.NFTs}>
         <Skeleton loading={!NFTBalances?.result}>
           {NFTBalances?.result &&
             NFTBalances.result.map((nft, index) => {
-              //Verify Metadata
+              // verify Metadata
               nft = verifyMetadata(nft);
+
+              // if (index === 0) {
+              //   contractOwner = isContractOwner(nft);
+              // } else {
+              //   console.log(
+              //     `Local Contract Owner: ${JSON.parse(
+              //       window.localStorage.getItem("contractOwner")
+              //     )}`
+              //   );
+              // }
+
               return (
                 <Card
                   hoverable
                   actions={[
+                    <Tooltip title="View Unlockable Content">
+                      <UnlockOutlined
+                        onClick={() => handleLockContentClick(nft)}
+                      />
+                    </Tooltip>,
+                    <Tooltip title="Create Unlockable Content">
+                      <LockOutlined
+                        onClick={() => handleLockContentClick(nft)}
+                      />
+                    </Tooltip>,
                     <Tooltip title="View On Blockexplorer">
                       <FileSearchOutlined
                         onClick={() =>
@@ -95,16 +321,8 @@ function NFTBalance() {
                         }
                       />
                     </Tooltip>,
-                    <Tooltip title="Transfer NFT">
-                      <SendOutlined onClick={() => handleTransferClick(nft)} />
-                    </Tooltip>,
-                    <Tooltip title="Sell On OpenSea">
-                      <ShoppingCartOutlined
-                        onClick={() => alert("OPENSEA INTEGRATION COMING!")}
-                      />
-                    </Tooltip>,
                   ]}
-                  style={{ width: 240, border: "2px solid #e7eaf3" }}
+                  style={{ width: "auto", border: "2px solid #e7eaf3" }}
                   cover={
                     <Image
                       preview={false}
@@ -123,20 +341,45 @@ function NFTBalance() {
         </Skeleton>
       </div>
       <Modal
-        title={`Transfer ${nftToSend?.name || "NFT"}`}
-        visible={visible}
-        onCancel={() => setVisibility(false)}
-        onOk={() => transfer(nftToSend, amountToSend, receiverToSend)}
+        title={"Create Unlockable content"}
+        visible={lockVisibility}
+        onCancel={() => setLockVisibility(false)}
+        onOk={() => checkContractOwnership(lockContent)}
         confirmLoading={isPending}
-        okText="Send"
+        okText="Check Ownership"
       >
-        <AddressInput autoFocus placeholder="Receiver" onChange={setReceiver} />
-        {nftToSend && nftToSend.contract_type === "erc1155" && (
-          <Input
-            placeholder="amount to send"
-            onChange={(e) => handleChange(e)}
-          />
-        )}
+        <Text style={{ display: "flex", margin: "0 0 15px 0" }}>
+          Enter your Contract Address so we can check you own the contract. Then
+          you can create Unlockable content for your whole collection.
+        </Text>
+        <Input
+          autoFocus
+          placeholder="Your Contract Address"
+          onChange={(e) => handleLockContentChange(e)}
+          showCount
+          maxLength={42}
+          value={
+            lockContent && lockContent.hasOwnProperty("token_address")
+              ? lockContent.token_address
+              : ""
+          }
+        />
+      </Modal>
+      <Modal
+        title={"Success!! Start creating Unlockable content"}
+        visible={createContentVisibility}
+        onCancel={() => setCreateContentVisibility(false)}
+        onOk={() => createUnlockableContent(editorSummaryContent)}
+        confirmLoading={isPending}
+        okText="Create Content"
+      >
+        <Text strong>Collection Summary</Text>
+        <ReactQuill
+          theme="snow"
+          onChange={setEditorSummaryContent}
+          placeholder={"Add a summary for your collection of NFTs.."}
+          value={editorSummaryContent || ""}
+        />
       </Modal>
     </div>
   );
