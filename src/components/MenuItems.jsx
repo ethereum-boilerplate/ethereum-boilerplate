dd8b9f5d814c2858382e13e7e24e5bf40680bdb3
michaeljwright <mike.filmworks@gmail.com>
2022-01-30 20:07:18 +0000

    Removed previous menu / nav options

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -11,36 +11,16 @@ function MenuItems() {
       mode="horizontal"
       style={{
         display: "flex",
-        fontSize: "17px",
-        fontWeight: "500",
+        fontSize: "20px",
+        fontWeight: "700",
         width: "100%",
-        justifyContent: "center",
+        justifyContent: "left",
+        marginLeft: "-15px",
       }}
       defaultSelectedKeys={[pathname]}
     >
-      <Menu.Item key="/quickstart">
-        <NavLink to="/quickstart">🚀 Quick Start</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/wallet">
-        <NavLink to="/wallet">👛 Wallet</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/1inch">
-        <NavLink to="/1inch">🏦 Dex</NavLink>
-      </Menu.Item>
-      <Menu.Item key="onramp">
-        <NavLink to="/onramp">💵 Fiat</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">💰 Balances</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/nftBalance">
-        <NavLink to="/nftBalance">🖼 NFTs</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/contract">
-        <NavLink to="/contract">📄 Contract</NavLink>
+      <Menu.Item style={{ padding: "0 8px" }} key="/">
+        <NavLink to="/">Unlockable</NavLink>
       </Menu.Item>
     </Menu>
   );
