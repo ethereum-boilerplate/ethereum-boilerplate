e79df368ae78505b7fec5968e88c619476ccb827
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-05-21 16:06:52 +0200

    fix: fix cors, testing piniata

--- src/components/Play/games/GymRoomSandbox.jsx
+++ src/components/Play/games/GymRoomSandbox.jsx
@@ -3,9 +3,9 @@ import GymRoom from "./GymRoom";
 
 const GymRoomSandbox = () => {
     const avatar = {
-        uri: 'https://ipfs.moralis.io:2053/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/images/0.png',
+        // uri: 'https://ipfs.moralis.io:2053/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/images/0.png',
         // uri: 'https://gateway.moralisipfs.com/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/images/0.png',
-        // uri: 'https://gateway.pinata.cloud/ipfs/QmXtFd8Fy1qnYchuY2bKLzcbfDrVRjoQCuWJzSLXio74yp',
+        uri: 'https://gateway.pinata.cloud/ipfs/QmXtFd8Fy1qnYchuY2bKLzcbfDrVRjoQCuWJzSLXio74yp',
         tokenAddress: '123',
         tokenId: '1',
     };
