44fdad79de671f54118020621e9f8b315ab48a4d
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-05-27 23:08:04 +0200

    chore: add fresh nft contract

--- src/MglNftMetadata.js
+++ src/MglNftMetadata.js
@@ -1,4 +1,4 @@
-export const TestGymBuddiesContract = "0x0650c5faeaec2193f7d0744cf90993277f530b74";
+export const TestGymBuddiesContract = "0xaf442b8249278126cc04038079a43f1721f48e1e";
 export const AvaxMoralis2021HackatonNfts = "0xbba97ea3912c598a39ce0802d5cd67dd3d873457";
 export const DemoNFT = "0x55bccbaf3d0cfd33b634c9c0df09ba2c14cc7c2a";
 export const AllowedNftContracts = new Map([
