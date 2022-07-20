dd2e334c675c249ce6f101a2c541c8ad205a2685
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-20 22:10:34 +0200

    chore: uregister service worker

--- src/index.js
+++ src/index.js
@@ -117,4 +117,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
