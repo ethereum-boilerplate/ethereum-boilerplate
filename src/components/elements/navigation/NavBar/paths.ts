cfccb3dcaf7a9421397a81a645ab944f4557f49d
Nio Martinez <niosupetranmartinez@gmail.com>
2022-08-30 19:35:55 +0800

    sec com

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -1,46 +1,18 @@
 import { ISubNav } from '../SubNav/SubNav';
 
 const NAV_LINKS: ISubNav[] = [
-  { label: 'Home', href: '/' },
+  { label: 'Featured', href: '/' },
+  {
+    label: 'Marketplace',
+    href: '/marketplace',
+  },
   {
     label: 'Transactions',
     href: '/transactions',
   },
   {
-    label: 'Transfers',
-    href: '/transfers',
-    children: [
-      {
-        label: 'ERC20',
-        subLabel: 'Get your ERC20 transfers',
-        href: '/transfers/erc20',
-        logo: 'token',
-      },
-      {
-        label: 'NFT',
-        subLabel: 'Get your ERC721 an ERC1155 transfers',
-        href: '/transfers/nft',
-        logo: 'lazyNft',
-      },
-    ],
-  },
-  {
-    label: 'Balances',
-    href: '/balances',
-    children: [
-      {
-        label: 'ERC20',
-        subLabel: 'Get your ERC20 balances',
-        href: '/balances/erc20',
-        logo: 'token',
-      },
-      {
-        label: 'NFT',
-        subLabel: 'Get your ERC721 an ERC1155 balances',
-        href: '/balances/nft',
-        logo: 'pack',
-      },
-    ],
+    label: 'Inventory',
+    href: '/inventory',
   },
 ];
 
---
+++ src/components/templates/featured/Featured.tsx
@@ -0,0 +1,13 @@
+import { Heading, VStack } from '@chakra-ui/react';
+
+const Featured = () => {
+  return (
+    <VStack w={'full'}>
+      <Heading size="md" marginBottom={6}>
+        Featured NFTs
+      </Heading>
+    </VStack>
+  );
+};
+
+export default Featured;
