1970ea238309730e5589bac63ed8e882f4d2228e
John Versus <johnvs.nagendra@gmail.com>
2022-08-30 19:23:27 +0530

    feat: added search bar
    added SearchBar component and updated Navbar file

--- src/components/elements/navigation/NavBar/NavBar.tsx
+++ src/components/elements/navigation/NavBar/NavBar.tsx
@@ -1,13 +1,28 @@
 import { HStack } from '@chakra-ui/react';
+import { SearchBar } from 'components/modules';
 import { NavItem } from '../NavItem';
 import NAV_LINKS from './paths';
+import { useState } from 'react';
+import { Button } from '@chakra-ui/react';
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
 
 const NavBar = () => {
-  return (
+  const [showInput, setShowInput] = useState<boolean>(false);
+  return showInput ? (
+    <HStack gap={'2px'}>
+      <SearchBar />
+      <Button colorScheme="teal" variant="ghost" onClick={() => setShowInput(false)}>
+        <CloseIcon fontSize={'sm'} />
+      </Button>
+    </HStack>
+  ) : (
     <HStack gap={'15px'}>
       {NAV_LINKS.map((link) => (
         <NavItem key={`link-${link.label}`} {...link} />
       ))}
+      <Button colorScheme="teal" variant="ghost" onClick={() => setShowInput(true)}>
+        <SearchIcon />
+      </Button>
     </HStack>
   );
 };
