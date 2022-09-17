c8123377e9154e954cdeddc6341f213c4e31bc80
3scava1i3r <tripathi.hritwik@gmail.com>
2022-09-17 21:57:39 +0530

    adding basic tokens

---
+++ hardhat/contracts/MageNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MageNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+    
+    constructor() ERC721("MageNFT", "MNFT") {}
+    
+    function mintTo(address recipient)
+        public
+        returns (uint256)
+    {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+}
+
