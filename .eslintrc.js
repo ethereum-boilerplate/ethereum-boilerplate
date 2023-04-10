981ed94a9a47c2c6d850d5f7612b32301bfe1fa7
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-04-10 20:54:33 +0700

    ✅ Basic Cypress tests added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@moralisweb3', 'plugin:@next/next/recommended'],
+  extends: ['@moralisweb3', 'plugin:@next/next/recommended', 'plugin:cypress/recommended'],
   ignorePatterns: ['**/build/**/*'],
   rules: {
     'no-console': 'off',
